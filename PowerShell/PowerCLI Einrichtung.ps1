483a8ffcc142baf5a8e78a5dfed84565364fd7a5
Marcus Andreas Föllng <vmware@foelling.net>
2022-05-12 09:36:38 +0200

    PowerCLI Einrichtung initial

---
+++ PowerShell/PowerCLI Einrichtung.ps1
@@ -0,0 +1,2 @@
+Set-PowerCLIConfiguration -Scope AllUsers -InvalidCertificateAction ignore -ProxyPolicy NoProxy -DefaultVIServerMode Multiple -WebOperationTimeoutSeconds 900 -ParticipateInCeip $false
+
