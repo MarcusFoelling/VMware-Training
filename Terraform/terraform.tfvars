02481ed35e52a2a39d98586e894e20c554f6ad28
Marcus Andreas Föllng <vmware@foelling.net>
2022-05-12 16:52:56 +0200

    vSwitch2.tf

---
+++ Terraform/terraform.tfvars
@@ -0,0 +1,3 @@
+vsphere_server = "sa-vcsa-01.vclass.local"
+vsphere_user = "administrator@vsphere.local"
+vsphere_password = "VMware1!"
--- Terraform/vSwitch2.tf
+++ Terraform/vSwitch2.tf
@@ -1,3 +1,10 @@
+provider "vsphere" {
+  user           = var.vsphere_user
+  password       = var.vsphere_password
+  vsphere_server = var.vsphere_server
+  allow_unverified_ssl = true
+}
+
 data "vsphere_datacenter" "datacenter" {
   name = "ICM-Datacenter"
 }
@@ -15,4 +22,18 @@ resource "vsphere_host_virtual_switch" "switch" {
 
   active_nics  = ["vmnic2"]
   standby_nics = []
+}
+
+resource "vsphere_host_port_group" "pg" {
+  name                = "vMotion"
+  host_system_id      = "${data.vsphere_host.host.id}"
+  virtual_switch_name = "${vsphere_host_virtual_switch.switch.name}"
+}
+
+resource "vsphere_vnic" "v1" {
+  host      = "${data.vsphere_host.host.id}"
+  portgroup = vsphere_host_port_group.pg.name
+  ipv4 {
+    dhcp = true
+  }
 }
