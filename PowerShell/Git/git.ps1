158f75652955d70c9090ee66d6fbfca9b9704f1f
Marcus Andreas Föllng <vmware@foelling.net>
2022-05-12 09:26:24 +0200

    initial git.ps1

---
+++ PowerShell/Git/git.ps1
@@ -0,0 +1,6 @@
+git config --global user.email "vmware@foelling.net"
+git config --global user.name "Marcus Andreas Fölling"
+
+& 'C:\Program Files (x86)\Git\bin\git.exe' remote add origin https://github.com/MarcusFoelling/VMware-Training.git
+& 'C:\Program Files (x86)\Git\bin\git.exe' branch -M main
+& 'C:\Program Files (x86)\Git\bin\git.exe' git push -u origin main
